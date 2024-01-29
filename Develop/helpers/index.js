ece624547b3a3319d5861dca62e0219525ac0ae8
Msteven <msdavid55@gmail.com>
2024-01-29 02:09:23 -0600

    added functionality to server.js to append notes to db.json

--- Develop/helpers/index.js
+++ Develop/helpers/index.js
@@ -1,42 +1,28 @@
-const noteTitle =document.querySelector('.note-title')
-const noteText = document.querySelector('.note-textarea')
-// const noteForm = document.querySelector('form')
-const saveBtn = document.querySelector('.save-note')
-const fs = require('fs')
+const fs = require('fs/promises');
 
-const saveNote = () => {
-saveBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-  
-    fetch('/api/notes', {
-      method: 'POST',
-      body: JSON.stringify({
-        title: noteTitle.value.trim(),
-        text: noteText.value.trim(),
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(res => res.json())
-    .then(data => fs.appendFile("db.json", data, (err) =>
-    err
-      ? console.error(err)
-      : console.log(
-          `Review for ${newReview.product} has been written to JSON file`
-        )))
-    .catch((err) => console.error(err));
-  })
-}
 
 const uuid = () => {
     return Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1);
+        .toString(16)
+        .substring(1);
 };
 
+async function readFromFile() {
+    let data = await fs.readFile('./db/db.json', 'utf8')
+    return JSON.parse(data);
+}
 
-module.export {
+async function appendToFile(data) {
+    try {
+        return await fs.appendFile('./db/db.json', JSON.stringify(data))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
+module.exports = {
     uuid,
-    saveNote
+    readFromFile,
+    appendToFile
 }
--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const noteData = require('./db/db.json')
 const PORT = 3001;
 const app = express();
-
+const fs = require('fs/promises')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,29 +15,37 @@ app.get('/notes', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
- res.json(noteData) 
+    res.json(noteData)
 })
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', async (req, res) => {
     // Log that a POST request was received
-    console.info(`${req.method} request received to add a review`);
+    console.info(`${req.method} request received to add a note`);
     let response;
 
-  // Check if there is anything in the response body
-  if (req.body.title && req.body.text) {
-    response = {
-      title: req.body.title,
-      text: req.body.text,
+    // Check if there is anything in the response body
+    if (req.body.title && req.body.text) {
+        response = {
+            title: req.body.title,
+            text: req.body.text,
+        };
+        try {
+            const currentNotesContent = await fs.readFile('./db/db.json', 'utf8');
+            const currentNotes = JSON.parse(currentNotesContent);
+
+            currentNotes.push(response);
+
+            await fs.writeFile('./db/db.json', JSON.stringify(currentNotes));
+
+            res.status(201).json(response);
+        } catch {
+            res.status(400).json('Request body must at least contain a title and content.');
+        }
+
+        // Log the response body to the console
+        console.log(req.body);
     };
-    res.status(201).json(response);
-  } else {
-    res.status(400).json('Request body must at least contain a title and content.');
-  }
-
-  // Log the response body to the console
-  console.log(req.body);
-});
-
+})
 
 app.listen(PORT, () => {
     console.log(`Notes application listening at http://localhost:${PORT}`)
